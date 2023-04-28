a441d77241f0b49d9e7a2c9b20e7f92e7b33193f
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2023-04-28 07:28:33 -0600

    Update Push Button.ino

--- Push Button.ino
+++ Push Button.ino
@@ -1,11 +1,11 @@
 
-int button_Pin = 13;              // set button in pin 13 (pin does not need to be PWM)
+int button_Pin = 13; // set button in pin 13 (pin does not need to be PWM)
 
 
 void setup() {
   
-  Serial.begin(9600);                         // set up serial monotor
-  pinMode(button_Pin, INPUT_PULLUP);          // set up button (define as input)
+  Serial.begin(9600);                // set up serial monotor
+  pinMode(button_Pin, INPUT_PULLUP); // set up button (define as input)
 
 }
 
@@ -13,11 +13,11 @@ void loop() {
 
 if (digitalRead(button_Pin) == LOW) { // if the buton is presed
   Serial.println("Presed!");          // print Presed! to the serial moitor
-
-} else {                              // if the button is not presed
-  Serial.println("Not presed");       // print Nope! to ther serial monitor
+  
+} else { // if the button is not presed
+  Serial.println("Not presed"); // print Nope! to ther serial monitor
 }
 
-delay(100);  // wait 0.1 seconds
+delay(100); // wait 0.1 seconds
 
 }
